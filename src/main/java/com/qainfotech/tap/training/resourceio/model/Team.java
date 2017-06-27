3aea85d3b2c977d5b0dae402f9ff267c6dbcca2e
piyusharoraQA <piyusharora@qainfotech.com>
2017-06-27 16:49:15 +0530

    Update Team.java

--- src/main/java/com/qainfotech/tap/training/resourceio/model/Team.java
+++ src/main/java/com/qainfotech/tap/training/resourceio/model/Team.java
@@ -1,6 +1,7 @@
 package com.qainfotech.tap.training.resourceio.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -9,57 +10,108 @@ import java.util.Map;
  * @author Ramandeep RamandeepSingh AT QAInfoTech.com
  */
 public class Team {
-    
+
     private final String name;
     private final Integer id;
     private final List<Individual> members;
-    
-    public Team(Map<String, Object> teamMap){
-        throw new UnsupportedOperationException("Not implemented.");
+
+    public Team(Map<String, Object> teamMap) {
+        String name1 = null;
+        Integer id1 = null;
+        List<Individual> members1 = null;
+
+        for (Map.Entry<String, Object> entry : teamMap.entrySet()) {
+            if (entry.getKey() == "name") {
+                name1 = entry.getValue().toString();
+            }
+            if (entry.getKey() == "id") {
+                id1 = (Integer) entry.getValue();
+            }
+            if (entry.getKey() == "members") {
+                members1 = (List<Individual>) entry.getValue();
+            }
+
+        }
+        name = name1;
+        id = id1;
+        members = members1;
+
     }
-    
+
     /**
      * get team name
-     * 
-     * @return 
+     *
+     * @return
      */
-    public String getName(){
+    public String getName() {
         return name;
     }
-    
+
     /**
      * get team id
-     * 
-     * @return 
+     *
+     * @return
      */
-    public Integer getId(){
+    public Integer getId() {
         return id;
     }
-    
-    /** 
+
+    /**
      * get list of individuals that are members of this team
-     * 
-     * @return 
+     *
+     * @return
      */
-    public List<Individual> getMembers(){
+    public List<Individual> getMembers() {
         return members;
     }
-    
+
     /**
-     * get a list of individuals that are members of this team and are also active
-     * 
-     * @return 
+     * get a list of individuals that are members of this team and are also
+     * active
+     *
+     * @return
      */
-    public List<Individual> getActiveMembers(){
-        throw new UnsupportedOperationException("Not implemented.");
+    public List<Individual> getActiveMembers() {
+
+        List<Individual> list1 = new ArrayList<>();
+        Iterator<Individual> itr = members.iterator();
+        while (itr.hasNext()) {
+            Individual ind = itr.next();
+            Boolean a = true;
+
+            Boolean b = ind.isActive();
+
+            if (a == b) {
+
+                list1.add(ind);
+            }
+
+        }
+
+        return list1;
     }
-        
+
     /**
      * get a list of individuals that are members of this team but are inactive
-     * 
-     * @return 
+     *
+     * @return
      */
-    public List<Individual> getInactiveMembers(){
-        throw new UnsupportedOperationException("Not implemented.");
+    public List<Individual> getInactiveMembers() {
+
+        List<Individual> list1 = new ArrayList<>();
+
+        Iterator<Individual> itr = members.iterator();
+        while (itr.hasNext()) {
+            Individual ind = itr.next();
+            Boolean a = false;
+            Boolean b = ind.isActive();
+
+            if (a == b) {
+                list1.add(ind);
+            }
+
+        }
+        return list1;
+
     }
 }
