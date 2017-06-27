d79676c7fbfc1e0be4af994bd035a55c7c414dac
piyusharoraQA <piyusharora@qainfotech.com>
2017-06-27 16:48:44 +0530

    Update Individual.java

--- src/main/java/com/qainfotech/tap/training/resourceio/model/Individual.java
+++ src/main/java/com/qainfotech/tap/training/resourceio/model/Individual.java
@@ -7,38 +7,58 @@ import java.util.Map;
  * @author Ramandeep RamandeepSingh AT QAInfoTech.com
  */
 public class Individual {
-    
+
     private final String name;
     private final Integer id;
     private final Boolean active;
-    
-    public Individual(Map<String, Object> individualMap){
-        throw new UnsupportedOperationException("Not implemented.");
+
+    public Individual(Map<String, Object> individualMap) {
+        String name1 = null;
+        Integer id1 = null;
+        Boolean active1 = null;
+        for (Map.Entry<String, Object> entry : individualMap.entrySet()) {
+            if (entry.getKey() == "name") {
+                name1 = entry.getValue().toString();
+            }
+            if (entry.getKey() == "id") {
+                id1 = (Integer) entry.getValue();
+            }
+            if (entry.getKey() == "active") {
+                active1 = (Boolean) entry.getValue();
+            }
+
+        }
+        name = name1;
+        id = id1;
+        active = active1;
+
     }
-    
+
     /**
      * get the name of individual
-     * 
+     *
      * @return individual name
      */
-    public String getName(){
+    public String getName() {
         return name;
     }
-    
+
     /**
      * get the employee of of individual
+     *
      * @return id
      */
-    public Integer getId(){
+    public Integer getId() {
         return id;
     }
-    
+
     /**
      * get the active status of individual
-     * 
-     * @return 
+     *
+     * @return
      */
-    public Boolean isActive(){
+    public Boolean isActive() {
+
         return active;
     }
 }
